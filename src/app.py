6b4b458b8a6f528e9412ac34052e8ecb8d3e9b57
marfurta <anamariamarfurt@gmail.com>
2022-08-06 20:21:32 +0000

    probando la app

--- src/app.py
+++ src/app.py
@@ -31,98 +31,36 @@ df = pd.read_csv(url, header=0, sep=",")
 
 # Clean the  DataSet
 
-#insulina
-df_0=df[(df['Outcome']==0) & (df["Insulin"] > 0)]
-insuline_mean_0=df_0['Insulin'].mean()
+#if value =0 and diabetic, set the mean of diabetic, else if value=0 and not diabetic, set the mean of not diabetic, 
+# rest of the case use the data value
 
-df_no0=df[(df['Outcome']!=0) & (df["Insulin"] > 0)]
-insuline_mean_no0=df_no0['Insulin'].mean()
-
-def insulina(insulin_value, outcome_value, insuline_mean_0,insuline_mean_no0):
-    if outcome_value==0 and insulin_value==0:
-        return insuline_mean_0
-    elif outcome_value==1 and insulin_value==0:
-        return insuline_mean_no0
+def set_value (data_value, outcome_value,mean_nodiab,mean_diab):
+    if (outcome_value == 0 and data_value==0):
+        return mean_nodiab
+    elif (outcome_value ==1 and data_value ==0):
+        return mean_diab
     else:
-        return insulin_value
+        return data_value
 
-df['Insulin'] = df.apply(lambda x: insulina(x['Insulin'], x['Outcome'],insuline_mean_0,insuline_mean_no0), axis=1)
+def set_use_mean (name_col):
+    #calc the mean for diabetic and not diabetic that the data is not 0
+    meanNoDiab = df[(df[name_col]>0) & (df['Outcome']==0)][name_col].mean()
+    meanDiab = df[(df[name_col]>0) & (df['Outcome']==1)][name_col].mean()
 
-#bmi
-df_0=df[(df['Outcome']==0) & (df["BMI"] > 0)]
-bmi_mean_0=df_0['BMI'].mean()
+    df[name_col] = df.apply(lambda x: set_value(x[name_col], x['Outcome'],meanNoDiab,meanDiab), axis=1)
 
-df_no0=df[(df['Outcome']!=0) & (df["BMI"] > 0)]
-bmi_mean_no0=df_no0['BMI'].mean()
+set_use_mean('Insulin')
+set_use_mean('Glucose')
+set_use_mean('BloodPressure')
+set_use_mean('SkinThickness')
+set_use_mean('BMI')
 
-def BMI_fun(bmi_value, outcome_value, bmi_mean_0,bmi_mean_no0):
-    if outcome_value==0 and bmi_value==0:
-        return bmi_mean_0
-    elif outcome_value==1 and bmi_value==0:
-        return bmi_mean_no0
-    else:
-        return bmi_value
-
-df['BMI'] = df.apply(lambda x: BMI_fun(x['BMI'], x['Outcome'],bmi_mean_0,bmi_mean_no0), axis=1)
-
-#Glucose
-df_0=df[(df['Outcome']==0) & (df["Glucose"] > 0)]
-glucose_mean_0=df_0['Glucose'].mean()
-
-df_no0=df[(df['Outcome']!=0) & (df["Glucose"] > 0)]
-glucose_mean_no0=df_no0['Glucose'].mean()
-
-def glucose_fun(glucose_value, outcome_value, glucose_mean_0,glucose_mean_no0):
-    if outcome_value==0 and glucose_value==0:
-        return glucose_mean_0
-    elif outcome_value==1 and glucose_value==0:
-        return glucose_mean_no0
-    else:
-        return glucose_value
-
-df['Glucose'] = df.apply(lambda x: glucose_fun(x['Glucose'], x['Outcome'],glucose_mean_0,glucose_mean_no0), axis=1)
-
-#BloodPressure
-df_0=df[(df['Outcome']==0) & (df["BloodPressure"] > 0)]
-BloodPressure_mean_0=df_0['BloodPressure'].mean()
-
-df_no0=df[(df['Outcome']!=0) & (df["BloodPressure"] > 0)]
-BloodPressure_mean_no0=df_no0['BloodPressure'].mean()
-
-def BloodPressure_fun(BloodPressure_value, outcome_value, BloodPressure_mean_0,BloodPressure_mean_no0):
-    if outcome_value==0 and BloodPressure_value==0:
-        return BloodPressure_mean_0
-    elif outcome_value==1 and BloodPressure_value==0:
-        return BloodPressure_mean_no0
-    else:
-        return BloodPressure_value
-
-df['BloodPressure'] = df.apply(lambda x: BloodPressure_fun(x['BloodPressure'], x['Outcome'],BloodPressure_mean_0,BloodPressure_mean_no0), axis=1)
-
-#SkinThickness
-df_0=df[(df['Outcome']==0) & (df["SkinThickness"] > 0)]
-SkinThickness_mean_0=df_0['SkinThickness'].mean()
-
-df_no0=df[(df['Outcome']!=0) & (df["SkinThickness"] > 0)]
-SkinThickness_mean_no0=df_no0['SkinThickness'].mean()
-
-def SkinThickness_fun(SkinThickness_value, outcome_value, SkinThickness_mean_0,SkinThickness_mean_no0):
-    if outcome_value==0 and SkinThickness_value==0:
-        return SkinThickness_mean_0
-    elif outcome_value==1 and SkinThickness_value==0:
-        return SkinThickness_mean_no0
-    else:
-        return SkinThickness_value
-
-df['SkinThickness'] = df.apply(lambda x: SkinThickness_fun(x['SkinThickness'], x['Outcome'],SkinThickness_mean_0,SkinThickness_mean_no0), axis=1)
-
-#Resample the data to make a more balanced dataset
-from sklearn.utils import resample
-df_majority = df[(df["Outcome"]==0)]
-df_minority = df[(df["Outcome"]==1)]
-df_minority_upsampled = resample(df_minority,replace=True,n_samples=500,random_state=42)
-df = pd.concat([df_minority_upsampled,df_majority])
-
+#exclude pregnancy and BloodPressure to feature model
+ 
+#X = df_raw[list(df_raw.columns[1:8])]
+X = df[['Glucose','SkinThickness','Insulin','BMI','Age','DiabetesPedigreeFunction']] 
+y = df[['Outcome']]
+ 
 #Eliminating Outliers with IQR Method
 q1 = df['Insulin'].quantile(0.25)
 q3 = df['Insulin'].quantile(0.75)
@@ -180,6 +118,40 @@ Y=df["Outcome"]
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 scaler.fit_transform(X)
+
+
+#use the model save with new data to predicts prima
+
+# load the model from disk
+loaded_model = pickle.load(open(filename, 'rb'))
+
+#Predict using the model 
+Glucose=120
+SkinThickness=23
+Insulin=215
+BMI=29
+Age=24
+DiabetesPedigreeFunction=0.520
+
+#predigo el target para los valores seteados
+print('Predicted Diabetic : \n', loaded_model.predict([[Glucose,SkinThickness,Insulin,BMI,Age,DiabetesPedigreeFunction]]))
+
+Glucose=134
+SkinThickness=30
+Insulin=74
+BMI=34
+Age=24
+DiabetesPedigreeFunction=0.75
+
+#predigo el target para los valores seteados
+print('Predicted Diabetic : \n', loaded_model.predict([[Glucose,SkinThickness,Insulin,BMI,Age,DiabetesPedigreeFunction]]))
+
+
+
+
+
+
+
 
 
 
